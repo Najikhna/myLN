06734ce7dfa9577e157cdb5b207bdaeb000c4ad1
Najikhna <157671850+Najikhna@users.noreply.github.com>
2026-05-13 01:21:26 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'myln-offline-v5'; 
+const CACHE_NAME = 'myln-offline-v6'; 
 const urlsToCache = ['/','/index.html','/style.css','/script.js','/manifest.json'];
 
 self.addEventListener('install', (event) => {
